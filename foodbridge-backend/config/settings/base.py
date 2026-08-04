5a1ef76aa7b670e62b6fbc27d003975d635823f9
VedantTuvar <vedanttuvar01@gmail.com>
2026-08-04 13:11:30 +0530

    Added

--- foodbridge-backend/config/settings/base.py
+++ foodbridge-backend/config/settings/base.py
@@ -85,8 +85,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'foodbridge_db'),
         'USER': os.environ.get('POSTGRES_USER', 'foodbridge_admin'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'secretpassword'),
-        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5433'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
